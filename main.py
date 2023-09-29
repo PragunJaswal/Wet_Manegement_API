2b1d24056ee3f36de2f12957d11452427f52b2c4
Pragun Jaswal <pragunjaswal2@gmail.com>
2023-09-30 00:31:05 +0530

    Asc

--- main.py
+++ main.py
@@ -51,6 +51,38 @@ templates =Jinja2Templates(directory="templates")
 def root():
     return{"server is running, Made by Pragun Jaswal"}
 
+# Define a function to periodically fetch and print the API response
+def print_api_response():
+    api_url = "https://swachhdhan.onrender.com/"
+    while True:
+        try:
+            # Make a GET request to the API
+            response = requests.get(api_url)
+
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                # Print the API response content
+                print(response.text)
+            else:
+                print(f"Failed to fetch data. Status code: {response.status_code}")
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+        # Wait for 5 seconds before making the next request
+        time.sleep(60)
+
+
+# Create a background thread to run the print_api_response function
+background_thread = threading.Thread(target=print_api_response)
+
+# Start the background thread when the application starts
+@app.on_event("startup")
+def on_startup():
+    background_thread.start()
+
+
+
 @app.get("/get/data/login")
 def getpost():
     cursor.execute("""SELECT * FROM public."Login database" ORDER BY id DESC """)
